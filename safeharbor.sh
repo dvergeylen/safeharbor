f301f0a89a35a3e43b50cc246f9998c7752ae3ba
Daniel Vergeylen <daniel.vergeylen@place2be.biz>
2017-03-23 11:18:59 +0100

    Added the other way round: delete repos that aren't starred anymore

--- safeharbor.sh
+++ safeharbor.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Define credentials
-GHUSER=YOUR_USERNAME
+GHUSER=GITHUB_USERNAME
 url="https://api.github.com/users/$GHUSER/starred"
+stars_list=()
 
-# Loop while there is pagination
-while [ ! -z "$url" ]
+# #############################################
+# Step 1: Clone and Update starred repositories
+# #############################################
+while [ ! -z "$url" ] # Loop while there is a pagination URL
 do
   # Fetch pagination
   output=$(curl -is "$url")
@@ -18,6 +21,7 @@ do
       dirname=$(echo "$repo" | cut -d"/" -f5)
 
       if [[ ! -z "$dirname" ]]; then
+
         if [[ ! -d "$dirname" ]]; then
           git clone "$repo" # First time? Clone repo first
         fi
@@ -27,10 +31,23 @@ do
         for b in $(git branch -r | grep -v -- '->'); do git branch --track "${b##origin/}" "$b"; done
         git fetch --all
         popd
+
+        # Append Repo name prevent later removal
+        stars_list+=("$dirname")
       fi
     fi
   done
 
   # Find the next pagination URL
   url=$(echo "$output" | grep 'rel="next"' | cut -d';' -f1 | grep -Po "\<\K\S+" | tr -d '>')
+done
+
+# ###############################################
+# Step 2: Remove repositories not starred anymore
+# ###############################################
+repo_list=($(ls))
+diff_list=($(echo ${stars_list[@]} ${repo_list[@]} | tr ' ' '\n' | sort | uniq -u))
+
+for dir in "${diff_list[@]}"; do
+  rm -Rf dir
 done
